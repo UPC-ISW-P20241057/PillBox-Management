af40df208df8e0da52a0faaa851e673f145ed51d
JackelineSalome <89036070+JackelineSalome@users.noreply.github.com>
2024-06-08 00:00:45 -0500

    feat: new methods

--- src/main/java/com/medibox/PillBox/weight/service/DataServiceImpl.java
+++ src/main/java/com/medibox/PillBox/weight/service/DataServiceImpl.java
@@ -31,31 +31,48 @@ public class DataServiceImpl implements DataService {
       newData.setWifi(false);
       dataRepository.save(newData);
       data.add(newData);
+    } else {
+      data.forEach(this::ensureNonNullFields);
     }
     return data;
   }
 
-
-
   @Override
   public Data update(Data data) {
+    ensureNonNullFields(data);
     return dataRepository.save(data);
   }
 
   @Override
   public Data getLatestWeight() {
-    return dataRepository.findTopByOrderByIdDesc();
+    Data latestData = dataRepository.findTopByOrderByIdDesc();
+    ensureNonNullFields(latestData);
+    return latestData;
   }
 
   @Override
   public Data updateWeight(Long id, String value) {
     Data data = dataRepository.findById(id).orElseThrow(() -> new RuntimeException("Data not found"));
     data.setValue(value);
+    ensureNonNullFields(data);
     return dataRepository.save(data);
   }
 
   @Override
   public Optional<Data> getById(Long id) {
-    return dataRepository.findById(id);
+    Optional<Data> dataOptional = dataRepository.findById(id);
+    dataOptional.ifPresent(this::ensureNonNullFields);
+    return dataOptional;
+  }
+
+  private void ensureNonNullFields(Data data) {
+    if (data.getValue() == null) data.setValue("0");
+    if (data.getReminder() == null) data.setReminder(false);
+    if (data.getIsEmpty() == null) data.setIsEmpty(false);
+    if (data.getAlmostEmpty() == null) data.setAlmostEmpty(false);
+    if (data.getNumberAlarm() == null) data.setNumberAlarm(1);
+    if (data.getSsid() == null) data.setSsid("");
+    if (data.getPassword() == null) data.setPassword("");
+    if (data.getWifi() == null) data.setWifi(false);
   }
 }
